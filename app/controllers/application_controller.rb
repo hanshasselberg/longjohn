0b812e927a34296794827500e6e858cb701f655d
Hans Hasselberg <me@hans.io>
2012-02-12 15:08:20 +0100

    Add locale parameter.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # FIXME: strange problems on creating new reservations... :(
   # protect_from_forgery
+  before_filter :set_locale
 
   expose(:equipment_reservations) do
     current_user.equipment_reservations.sort_by{ |r| r.from }.map{|r| EquipmentReservationDecorator.new(r)}
@@ -16,5 +17,9 @@ class ApplicationController < ActionController::Base
 
   def requires_login
     redirect_to log_in_path unless current_user
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
