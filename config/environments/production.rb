315a46588b02530c0caf21ef08b14a9c1a9b39fa
Hans Hasselberg <me@hans.io>
2012-02-13 14:44:37 +0100

    Change production to german.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,7 @@ Longjohn::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
+  config.i18n.default_locale = :de
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
