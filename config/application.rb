1247165f3054fb138e1e0d563382be433241f1a7
Hans Hasselberg <me@hans.io>
2012-04-23 10:43:14 +0200

    Readd sprockets in order to compile assets on heroku.

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,11 @@ require File.expand_path('../boot', __FILE__)
 
 require "action_controller/railtie"
 require "active_resource/railtie"
+require "sprockets/railtie" # Uncomment this line for Rails 3.1+
 
 
 if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
   Bundler.require *Rails.groups(:assets => %w(development test))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
 end
 
 module Longjohn
